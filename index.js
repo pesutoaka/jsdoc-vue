0f00ff7ddd31be2502253a523c0be1ef8c905c1f
Kamil Kulinski <kamil@inovorobotics.com>
2020-06-09 10:36:38 +0100

    Fix for the correct line for the source-code preview.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var compiler = require('vue-template-compiler');
 exports.handlers = {
   beforeParse: function (e) {
     if (/\.vue$/.test(e.filename)) {
-      var output = compiler.parseComponent(e.source);
+      var output = compiler.parseComponent(e.source,{ pad: 'line' });
 
       e.source = output.script ? output.script.content : '';
     }
